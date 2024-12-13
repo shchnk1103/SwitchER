b02016e64935cc52459fe52f0f03076b07995342
DoubleShy0N <doubleshy0n@DoubleShy0Ns-MacBook-Pro-7.local>
2024-12-13 14:41:11 +0800

    init

---
+++ switcher/switcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  switcherApp.swift
+//  switcher
+//
+//  Created by DoubleShy0N on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct switcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
